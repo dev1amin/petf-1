c0e24d42332aeb0a238eb4090bee3a0a82033f27
rafaeliannini <ianninirafael@gmail.com>
2024-09-25 23:34:17 -0300

    produto e index completo

--- cadastroProduto/cadastro.js
+++ cadastroProduto/cadastro.js
@@ -4,24 +4,20 @@ document.getElementById('formCadastro').addEventListener('submit', function(e) {
     var nomeProduto = document.getElementById('nomeProduto').value;
     var urlImagem = document.getElementById('urlImagem').value;
 
-    // Adicionando um campo 'id' fictício para demonstração
+    // Adicionando um campo 'id' fictício para o novo produto
     var novoProduto = { id: Date.now(), nome: nomeProduto, imagem: urlImagem };
 
-    // Substituindo localStorage pela chamada à API
-    fetch('https://api-render-pet.onrender.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(novoProduto)
-    })
-  .then(response => response.json())
-  .then(data => {
-        if (data.success) {
-            alert('Produto cadastrado com sucesso. Agora, atualize a página principal.');
-        } else {
-            alert('Produto cadastrado com sucesso.');
-        }
-    })
-  .catch(error => console.error('Erro:', error));
+    // Recuperando produtos existentes do localStorage
+    var produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+
+    // Adicionando o novo produto à lista
+    produtos.push(novoProduto);
+
+    // Armazenando a lista atualizada no localStorage
+    localStorage.setItem('produtos', JSON.stringify(produtos));
+
+    alert('Produto cadastrado com sucesso. Agora, atualize a página principal.');
+
+    // Limpa o formulário após o cadastro
+    document.getElementById('formCadastro').reset();
 });
